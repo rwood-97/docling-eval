11363ea8602380fdc61dbd60622e0b6e749577d0
Praveen Kumar Midde <praveen.midde@in.ibm.com>
2025-03-21 17:55:50 +0530

    Remove unused code

--- docling_eval_next/dataset_builders/fintabnet_builder.py
+++ docling_eval_next/dataset_builders/fintabnet_builder.py
@@ -2,11 +2,11 @@ import glob
 import io
 import json
 import os
-from datasets import load_dataset
 from io import BytesIO
 from pathlib import Path
 from typing import Any, Dict, Iterable, List
 
+from datasets import load_dataset
 from docling_core.types import DoclingDocument
 from docling_core.types.doc import (
     BoundingBox,
@@ -31,7 +31,7 @@ from docling_eval.benchmarks.utils import (
     from_pil_to_base64uri,
     get_binhash,
 )
-
+from docling_eval.converters.models.tableformer.tf_model_prediction import PageTokens
 from docling_eval.visualisation.visualisations import save_comparison_html
 from docling_eval_next.datamodels.dataset_record import DatasetRecord
 from docling_eval_next.dataset_builders.dataset_builder import (
@@ -40,9 +40,6 @@ from docling_eval_next.dataset_builders.dataset_builder import (
 )
 from docling_eval_next.prediction_providers.prediction_provider import (
     BasePredictionProvider,
-)
-from docling_eval.converters.models.tableformer.tf_model_prediction import (
-    PageTokens,
 )
 
 TRUE_HTML_EXPORT_LABELS = {
@@ -121,161 +118,6 @@ class FintabnetTableStructureDatasetBuilder(FintabnetDatasetBuilder):
             target=target,
             do_visualization=do_visualization,
         )
-
-    def _update_gt_doc(
-        self,
-        doc: DoclingDocument,
-        annots: Dict,
-        page,
-        page_image: Image,
-        page_width: float,
-        page_height: float,
-    ):
-
-        label = annots["category"]
-
-        min_x = annots["coordinates"][0]["x"]
-        max_x = annots["coordinates"][0]["x"]
-
-        min_y = annots["coordinates"][0]["y"]
-        max_y = annots["coordinates"][0]["y"]
-
-        for coor in annots["coordinates"]:
-            min_x = min(min_x, coor["x"])
-            max_x = max(max_x, coor["x"])
-
-            min_y = min(min_y, coor["y"])
-            max_y = max(max_y, coor["y"])
-
-        text = annots["content"]["text"].replace("\n", " ")
-        html = annots["content"]["html"]
-
-        bbox = BoundingBox(
-            l=min_x * page_width,
-            r=max_x * page_width,
-            t=min_y * page_height,
-            b=max_y * page_height,
-            coord_origin=CoordOrigin.TOPLEFT,
-        )
-
-        prov = ProvenanceItem(page_no=1, bbox=bbox, charspan=(0, len(text)))
-
-        img = crop_bounding_box(page_image=page_image, page=page, bbox=bbox)
-
-        if label == "Header":
-            doc.add_text(
-                label=DocItemLabel.PAGE_HEADER, text=text, orig=text, prov=prov
-            )
-
-        elif label == "Footer":
-            doc.add_text(
-                label=DocItemLabel.PAGE_FOOTER, text=text, orig=text, prov=prov
-            )
-
-        elif label == "Paragraph":
-            doc.add_text(label=DocItemLabel.TEXT, text=text, orig=text, prov=prov)
-
-        elif label == "Index":
-
-            # FIXME: ultra approximate solution
-            text = annots["content"]["text"]
-            rows = text.split("\n")
-
-            num_rows = len(rows)
-            num_cols = 2
-
-            row_span = 1
-            col_span = 1
-
-            cells = []
-            for row_idx, row in enumerate(rows):
-                parts = row.split(" ")
-
-                col_idx = 0
-                cell = TableCell(
-                    row_span=row_span,
-                    col_span=col_span,
-                    start_row_offset_idx=row_idx,
-                    end_row_offset_idx=row_idx + row_span,
-                    start_col_offset_idx=col_idx,
-                    end_col_offset_idx=col_idx + col_span,
-                    text=" ".join(parts[:-1]),
-                )
-                cells.append(cell)
-
-                col_idx = 1
-                cell = TableCell(
-                    row_span=row_span,
-                    col_span=col_span,
-                    start_row_offset_idx=row_idx,
-                    end_row_offset_idx=row_idx + row_span,
-                    start_col_offset_idx=col_idx,
-                    end_col_offset_idx=col_idx + col_span,
-                    text=parts[-1],
-                )
-                cells.append(cell)
-
-            table_data = TableData(
-                num_rows=num_rows, num_cols=num_cols, table_cells=cells
-            )
-            doc.add_table(
-                data=table_data,
-                caption=None,
-                prov=prov,
-                label=DocItemLabel.DOCUMENT_INDEX,
-            )
-
-        elif label == "List":
-            doc.add_list_item(text=text, orig=text, prov=prov)
-            # doc.add_text(label=DocItemLabel.TEXT, text=text, orig=text, prov=prov)
-
-        elif label == "Caption":
-            doc.add_text(label=DocItemLabel.CAPTION, text=text, orig=text, prov=prov)
-
-        elif label == "Equation":
-            doc.add_text(label=DocItemLabel.FORMULA, text=text, orig=text, prov=prov)
-
-        elif label == "Figure":
-            uri = from_pil_to_base64uri(img)
-
-            imgref = ImageRef(
-                mimetype="image/png",
-                dpi=72,
-                size=Size(width=img.width, height=img.height),
-                uri=uri,
-            )
-
-            doc.add_picture(prov=prov, image=imgref)
-
-        elif label == "Table":
-
-            table_data = convert_html_table_into_docling_tabledata(table_html=html)
-
-            doc.add_table(data=table_data, caption=None, prov=prov)
-
-        elif label == "Chart":
-            uri = from_pil_to_base64uri(img)
-
-            imgref = ImageRef(
-                mimetype="image/png",
-                dpi=72,
-                size=Size(width=img.width, height=img.height),
-                uri=uri,
-            )
-
-            doc.add_picture(prov=prov, image=imgref)
-
-            # doc.add_picture(prov=prov)
-
-        elif label == "Footnote":
-            doc.add_text(label=DocItemLabel.FOOTNOTE, text=text, orig=text, prov=prov)
-
-        elif label == "Heading1":
-            doc.add_heading(text=text, orig=text, level=1, prov=prov)
-
-        else:
-            return
-
 
     def create_page_tokens(self, data: List[Any], height: float, width: float) -> PageTokens:
         """Needed for tableformer model only, where it additionally needs the page tokens for extraction.
