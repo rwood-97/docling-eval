ace02af66ca907b8df0c4e420de4c5c120cc4fec
Christoph Auer <cau@zurich.ibm.com>
2025-04-08 13:30:08 +0200

    Port AWS Textract and Google DocAI providers, add tests
    Signed-off-by: Christoph Auer <cau@zurich.ibm.com>

---
+++ tests/test_tables_aws.py
@@ -0,0 +1,52 @@
+import os
+from pathlib import Path
+
+import pytest
+
+from docling_eval.cli.main import evaluate, visualize
+from docling_eval.datamodels.types import BenchMarkNames, EvaluationModality
+from docling_eval.dataset_builders.otsl_table_dataset_builder import (
+    FintabNetDatasetBuilder,
+)
+from docling_eval.prediction_providers.aws_prediction_provider import (
+    AWSTextractPredictionProvider,
+)
+
+IS_CI = os.getenv("RUN_IN_CI") == "1"
+
+
+@pytest.mark.skipif(
+    IS_CI, reason="Skipping test in CI because the dataset is too heavy."
+)
+def test_run_fintabnet_builder():
+    target_path = Path(f"./scratch/{BenchMarkNames.FINTABNET.value}/")
+    aws_provider = AWSTextractPredictionProvider(
+        do_visualization=True, ignore_missing_predictions=False
+    )
+
+    dataset = FintabNetDatasetBuilder(
+        target=target_path / "gt_dataset",
+        end_index=5,
+    )
+
+    dataset.save_to_disk()  # does all the job of iterating the dataset, making GT+prediction records, and saving them in shards as parquet.
+
+    aws_provider.create_prediction_dataset(
+        name=dataset.name,
+        gt_dataset_dir=target_path / "gt_dataset",
+        target_dataset_dir=target_path / "eval_dataset",
+    )
+
+    evaluate(
+        modality=EvaluationModality.TABLE_STRUCTURE,
+        benchmark=BenchMarkNames.FINTABNET,
+        idir=target_path / "eval_dataset",
+        odir=target_path / "evaluations" / EvaluationModality.TABLE_STRUCTURE.value,
+    )
+
+    visualize(
+        modality=EvaluationModality.TABLE_STRUCTURE,
+        benchmark=BenchMarkNames.FINTABNET,
+        idir=target_path / "eval_dataset",
+        odir=target_path / "evaluations" / EvaluationModality.TABLE_STRUCTURE.value,
+    )
